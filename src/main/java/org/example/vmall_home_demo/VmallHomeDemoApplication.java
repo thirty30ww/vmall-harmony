fe426f2a8a641b32d80e102b487a8ac19de28887
Merrick <2834379272@qq.com>
2026-07-06 10:20:27 +0800

    fix(cart): 纯本地操作的简单实现

--- src/main/java/org/example/vmall_home_demo/VmallHomeDemoApplication.java
+++ src/main/java/org/example/vmall_home_demo/VmallHomeDemoApplication.java
@@ -3,7 +3,7 @@ package org.example.vmall_home_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication897
 public class VmallHomeDemoApplication {
 
     public static void main(String[] args) {
